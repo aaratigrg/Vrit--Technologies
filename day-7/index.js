85f6b48d1e0cb7c4f7196b9cb317f9a5e27960b0
aaratigrg <164876775+aaratigrg@users.noreply.github.com>
2024-05-20 18:18:15 +0545

    Day-11 commit

---
+++ day-7/index.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counter = document.getElementById('counter');
+    const incrementButton = document.getElementById('increment');
+    const decrementButton = document.getElementById('decrement');
+    
+    let count = 0;
+
+    incrementButton.addEventListener('click', () => {
+        count++;
+        counter.textContent = count;
+    });
+
+    decrementButton.addEventListener('click', () => {
+        count--;
+        counter.textContent = count;
+    });
+});
